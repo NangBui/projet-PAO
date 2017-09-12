443aa40f30dfe543bf9459e8dd59fa1b7a9d10e3
toto <toto@gmail.com>
2017-09-12 14:05:44 +0200

    first commit

---
+++ main.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      // projet PAO
+      System.out.println("Hello World, debut projet PAO");
+   }
+}
